57b41a0e68dd8de55c0c5d30165c0f0b0a922688
Pradist Keawborisut <pradist.ke@gmail.com>
2024-02-03 19:12:27 +0700

    feat: add main for add user and get all

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/pradist/user/domain"
 	"github.com/pradist/user/repository"
 )
 
@@ -10,6 +11,13 @@ func main() {
 
 	r := repository.NewUserInMemoryRepository(nil)
 	users, _ := r.Get()
+
+	fmt.Printf("%v\n", users)
+
+	u := domain.User{ID: 3, Name: "Jane"}
+	r.Save(u)
+
+	users, _ = r.Get()
 
 	fmt.Printf("%v\n", users)
 }
